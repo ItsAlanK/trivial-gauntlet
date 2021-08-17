c36fd944c4e5437c049c0a6b218c4e41cdc19e57
Alan Kinsella <alankinsella1993@gmail.com>
2021-08-17 15:54:21 +0000

    Allow difficulty buttons to call getQuestions function which fetches an array of questions based on the difficulty selected

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,16 @@ document.addEventListener("DOMContentLoaded", function () {
     let difficultyBtns = document.getElementsByClassName("difficulty-btn");
 
     for (let button of difficultyBtns) {
-        button.addEventListener("click", function () {
-            getQuestions();
+        button.addEventListener("click", function (e) {
+            if (e.target.id == "easy-btn") {
+                getQuestions("easy");
+            } else if (e.target.id == "medium-btn") {
+                getQuestions("medium");
+            } else if (e.target.id == "hard-btn") {
+                getQuestions("hard");
+            } else {
+                alert("Difficulty Undefined");
+            }
             startGame();
         })
     }
@@ -12,13 +20,15 @@ document.addEventListener("DOMContentLoaded", function () {
 /**
  * Uses Open Trivia Database API to create Array of questions based on difficulty option selected
  */
-function getQuestions() {
-    alert("getquestions");
+async function getQuestions(difficultyChoice) {
+    let response = await fetch(`https://opentdb.com/api.php?amount=50&difficulty=${difficultyChoice}`);
+    let rawData = await response.json();
+    console.log(rawData);
 }
 
 /** 
  * Resets score and progress values and calls functions to start a new game
  */
 function startGame() {
-    alert("startGame");
+    
 }
