6d22b71f3192924898974226613fd0b3b1ef7691
Alan Kinsella <alankinsella1993@gmail.com>
2021-08-17 15:09:17 +0000

    Add function descriptions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+
+
 document.addEventListener("DOMContentLoaded", function() {
     let difficultyBtns = document.getElementsByClassName("difficulty-btn");
 
@@ -9,10 +11,16 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 })
 
+/**
+ * Uses Open Trivia Database API to create Array of questions based on difficulty option selected
+ */
 function getQuestions() {
     alert("getquestions");
 }
 
+/** 
+ * Resets score and progress values and calls functions to start a new game
+ */
 function startGame() {
     alert("startGame");
 }
