d87639dcdd3462b8c4f3338d0ffa479991278fac
Alan Kinsella <alankinsella1993@gmail.com>
2021-08-17 17:01:31 +0000

    Add function to request session token to append to questions request and move startGame function to correct location

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,40 @@
-document.addEventListener("DOMContentLoaded", function () { // check for content to be loaded
+document.addEventListener("DOMContentLoaded", async function () { // check for content to be loaded
     let difficultyBtns = document.getElementsByClassName("difficulty-btn");
+
+    let sessionToken = await getToken();
 
     for (let button of difficultyBtns) { // adds listeners to difficulty selection buttons
         button.addEventListener("click", function (e) { 
             if (e.target.id == "easy-btn") { // calls getQuestions function
-                getQuestions("easy");
+                getQuestions("easy", sessionToken);
+                startGame();
             } else if (e.target.id == "medium-btn") {
-                getQuestions("medium");
+                getQuestions("medium", sessionToken);
+                startGame();
             } else if (e.target.id == "hard-btn") {
-                getQuestions("hard");
+                getQuestions("hard", sessionToken);
+                startGame();
             } else {
                 alert("Difficulty Undefined");
             }
-            startGame();
         })
     }
 })
 
 /**
+ * Gets a session token on page load to ensure questions are not duplicated
+ */
+async function getToken() {
+    let response = await fetch("https://opentdb.com/api_token.php?command=request");
+    let token = await response.json();
+    return token.token;
+}
+
+/**
  * Uses Open Trivia Database API to create Array of questions based on difficulty option selected
  */
-async function getQuestions(difficultyChoice) {
-    let response = await fetch(`https://opentdb.com/api.php?amount=50&difficulty=${difficultyChoice}`);
+async function getQuestions(difficultyChoice, sessionToken) {
+    let response = await fetch(`https://opentdb.com/api.php?amount=50&difficulty=${difficultyChoice}&token=${sessionToken}`);
     let rawData = await response.json();
     console.log(rawData);
 }
